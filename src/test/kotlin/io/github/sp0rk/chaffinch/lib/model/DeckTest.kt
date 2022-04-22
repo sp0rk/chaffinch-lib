2cb18deff2d05c2d26228bb8273d0d9c1ebdacfe
Julian Jurec <julian.jurec@applover.pl>
2022-04-21 19:28:38 -0700

    Add deck and bird models

--- src/test/kotlin/io/github/sp0rk/chaffinch/lib/model/DeckTest.kt
+++ src/test/kotlin/io/github/sp0rk/chaffinch/lib/model/DeckTest.kt
@@ -4,15 +4,30 @@ import com.google.common.truth.Truth.assertThat
 import kotlin.test.Test
 
 internal class DeckTest {
+    private val bird = Bird("Black-capped chickadee", "https://example.com")
+    private val deck = Deck("Garden", listOf(bird))
+
     @Test
     fun `GIVEN Deck with name WHEN name is requested THEN return name`() {
         // given
-        val deck = Deck("test")
+        val deck = deck
 
         // when
         val name = deck.name
 
         // then
-        assertThat(name).isEqualTo("test")
+        assertThat(name).isEqualTo("Garden")
+    }
+
+    @Test
+    fun `GIVEN Deck with birds WHEN birds are requested THEN return birds`() {
+        // given
+        val deck = deck
+
+        // when
+        val bird = deck.birds.firstOrNull()
+
+        // then
+        assertThat(bird).isEqualTo(this.bird)
     }
 }
